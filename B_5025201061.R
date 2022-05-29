dc7044380c8390ea14c12ce19882801ae86b9064
rachmanridwan <90601251+rachmanridwan@users.noreply.github.com>
2022-05-29 21:33:16 +0700

    Add files via upload

---
+++ B_5025201061.R
@@ -0,0 +1,92 @@
+# No. 1
+library(BSDA)
+
+respon <- c(1,2,3,4,5,6,7,8,9)
+data_x <- c(78,75,67,77,70,72,78,74,77)
+data_y <- c(100,95,70,90,90,90,89,90,100)
+
+data_x
+data_y
+
+data <- data.frame(respon,data_x,data_y)
+standar_deviasi <- sd(data$data_x - data$data_y)
+
+print("hasil dari point A ")
+standar_deviasi
+
+h <- data$data_x - data$data_y
+n <- 6
+s <- sd(h[1:6])
+mean1 <- mean(h)
+zbar <- mean(h[1:6])
+t <- ((zbar - mean1)/(s/sqrt(n)))
+p <- (2*pt(-abs(t),df <- n-1))
+
+print("hasil dari point B ")
+t
+p
+
+t.test(h, alternative = 'two.sided',mu = mean1)
+# =====================================================
+
+
+# No. 2
+library(BSDA)
+
+print("hasil dari point A ")
+tsum.test(mean.x=23500, sd(3900), n.x=100)
+
+z <- (23500 - 20000)/(3900/sqrt(100))
+z
+# =====================================================
+
+
+# No. 3
+library(mosaic)
+
+print("hasil dari point A ")
+zH0 <- (3.64-0)/(1.67/sqrt(19))
+zH1 <- (2.79-0)/(1.32/sqrt(27))
+
+zH0
+zH1
+
+print("hasil dari point B ")
+tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19,
+          mean.y =2.79 , s.y = 1.32, n.y = 27,
+          alternative = "greater")
+
+print("hasil dari point C ")
+uji_statistik <- plotDist(dist='t',df=2,  col="red")
+uji_statistik
+
+print("hasil dari point D ")
+nilai_kritikal <- qchisq(p = 0.05, df = 2, lower.tail=FALSE)
+nilai_kritikal
+# =====================================================
+
+# No. 4
+print("hasil dari point A ")
+file_data <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"))
+attach(file_data)
+file_data$Jenis <- as.factor(file_data$Jenis)
+file_data$Jenis <- factor(file_data$Jenis,
+                          labels = c("kucing oren",
+                                     "kucing hitam",
+                                     "kucing putih"))
+class(file_data$Jenis)
+
+grup1 <- subset(file_data, Jenis == "kucing oren")
+grup2 <- subset(file_data, Jenis == "kucing hitam")
+grup3 <- subset(file_data, Jenis == "kucing putih")
+
+file_data
+grup1
+grup2
+grup3
+
+
+
+
+
+
